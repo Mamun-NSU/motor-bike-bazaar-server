9aa11b9abf0c34efd5864ddd729b4ee786bb152c
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-07-20 23:26:44 +0600

    Modified successfully

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get("/", (req, res) => {
 app.use(cors());
 app.use(express.json());
 
-
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
@@ -31,7 +30,6 @@ function verifyJWT(req, res, next) {
     next();
   })
 }
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sc4os.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -52,8 +50,6 @@ async function run() {
       });
       res.send({ accessToken });
     });
-
-    // After solving the problem
 
     app.get("/products", async (req, res) => {
       const query = {};
